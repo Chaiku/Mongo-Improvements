cd77ea0217d059e14a0640d2f78f00c106267f02
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-03-05 21:35:46 -0500

    initial heroku deply - still need db sync help

--- seeds/kudos-seed.js
+++ seeds/kudos-seed.js
@@ -5,7 +5,7 @@
 // mongoose.connect(
 //     process.env.MONGODB_URI || "mongodb://localhost/kudosdb");
 
-//     db.Kudo.insert({"sender": "Mitch", "receiver": "Joanne", "body": "You're the best!!"});
-//     db.Kudo.insert({"sender": "Juan", "receiver": "Mitch", "body":"Don't forget those TPS reports."});
-//     db.Kudo.insert({"sender": "Joanne", "receiver": "Greg", "body": "Those cookies were really good."});
+//     db.Kudo.collection.insert({"sender": "Mitch", "receiver": "Joanne", "body": "You're the best!!"});
+//     db.Kudo.collection.insert({"sender": "Juan", "receiver": "Mitch", "body":"Don't forget those TPS reports."});
+//     db.Kudo.collection.insert({"sender": "Joanne", "receiver": "Greg", "body": "Those cookies were really good."});
 
